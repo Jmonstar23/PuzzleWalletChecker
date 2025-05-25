a617a8c25c2326ac243b051ef0e64b171332351f
Jmonstar23 <JmonStar23@gmail.com>
2025-05-25 18:06:39 -0400

    <# Created local_config and migrated Local Vars #>
    Created local_config.py, migrated local variables from OG config.py to local_config.py, added import to config.py. To do: add local_config to gitignore, make public local_config template.

---
+++ local_config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Local variables
+"""
+# local_config.py
+
+PREFIX = ["head", "honey", "bitter", "find", "sock", "dash"]
+FREE_WORDS = ["bridge", "current", "tail", "bottom", "matter", "like", "arrive", "addict", "fish", "inch"]
+GROUP2_WORDS = ["segway", "allcoins", "crypto", "website", "breath", "fight", "passenger", "gimmick"]
+PROXY_CERT = "/Users/macbook/python/charles-ssl-proxying-certificate.pem"
